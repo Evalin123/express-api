32c6ef8d70eea0678973299e4cda881f23a427b4
Eva <evalin8@gmail.com>
2020-06-17 23:24:23 +0800

    mongoose and body-parser

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const keys = require('./config/keys');
 
 const app = express();
 const port = 5000;
 
+mongoose.connect(keys.mongoUri, {
+  useNewUrlParser: true
+})
+  .then(() => {
+    console.log(`mongoose connect db`)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   response.send('Hello');
