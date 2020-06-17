14780ea56a377f21de0bef22300d65b0f8bb1890
Eva <evalin8@gmail.com>
2020-06-17 23:12:32 +0800

    init express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello');
+});
